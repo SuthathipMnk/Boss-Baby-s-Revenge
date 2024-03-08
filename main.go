91a92398c8d20530769f5e0c94b6472d0e7c93b1
SuthathipMnk <suthathip.mnk@gmail.com>
2024-03-08 23:15:37 +0700

    limit length of input

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ func main() {
 	  Good boy: "SRSSRRR", "SSRR"*/
 	var shotAndRevenge string
 	fmt.Print("Shot and Revenge: ")
-	fmt.Scanf("%s", &shotAndRevenge)
+	fmt.Scanf("%1000000s", &shotAndRevenge)
 	if strings.HasPrefix(shotAndRevenge, "R") ||
 		strings.HasSuffix(shotAndRevenge, "S") ||
 		strings.Count(shotAndRevenge, "S") > strings.Count(shotAndRevenge, "R") {
 		fmt.Println("Bad boy")
-	} else {
+	} else if strings.Count(shotAndRevenge, "S") <= strings.Count(shotAndRevenge, "R") {
 		fmt.Println("Good boy")
+	} else {
+		fmt.Println("Invalid value")
 	}
 }
