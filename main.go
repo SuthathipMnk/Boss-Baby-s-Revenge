5b4729fd8e3408f4b498362bdd43b634efb51a48
SuthathipMnk <suthathip.mnk@gmail.com>
2024-03-09 01:18:29 +0700

    refactor

--- main.go
+++ main.go
@@ -10,44 +10,53 @@ const revenge string = "R"
 const goodBoy string = "Good boy"
 const badBoy string = "Bad boy"
 
+var output string = goodBoy
+
 func main() {
+	var input string
+	fmt.Print("Input: ")
+	fmt.Scanf("%1000000s", &input)
+	printOutput(input)
+}
+
+func printOutput(input string) {
 	/*Bad boy: "SSSRRRRS", "RSSRR", "SRRSSR"
 	  Good boy: "SRSSRRR", "SSRR"*/
-	var shotAndRevenge string
-	fmt.Print("Input: ")
-	fmt.Scanf("%1000000s", &shotAndRevenge)
-	var numOfShot = strings.Count(shotAndRevenge, shot)
-	var numOfRevenge = strings.Count(shotAndRevenge, revenge)
-	var result string = goodBoy
-	if strings.HasPrefix(shotAndRevenge, revenge) ||
-		strings.HasSuffix(shotAndRevenge, shot) ||
-		numOfShot > numOfRevenge {
-		result = badBoy
+	var numOfShot = strings.Count(input, shot)
+	var numOfRevenge = strings.Count(input, revenge)
+
+	if strings.HasPrefix(input, revenge) || strings.HasSuffix(input, shot) || numOfShot > numOfRevenge {
+		output = badBoy
 	} else if numOfShot == numOfRevenge {
-		var countShots int = 0
-		var countRevenges int = 0
-		var preCharacter string
-		for i := 0; i < len(shotAndRevenge); i++ {
-			var currentCharacter string = string(shotAndRevenge[i])
-			if currentCharacter != preCharacter && countShots != 0 && countRevenges != 0 {
-				if countShots > countRevenges {
-					break
-				}
-
-				countShots = 0
-				countRevenges = 0
-			}
-
-			if currentCharacter == shot {
-				countShots++
-			} else if currentCharacter == revenge {
-				countRevenges++
-			}
-			preCharacter = string(shotAndRevenge[i])
-		}
-		if countShots > countRevenges {
-			result = badBoy
-		}
+		checkCaseShotEqualRevenge(input)
 	}
-	fmt.Println("Output: ", result)
+
+	fmt.Println("Output: ", output)
+}
+
+func checkCaseShotEqualRevenge(input string) {
+	var countShots int = 0
+	var countRevenges int = 0
+	var preCharacter string
+	for i := 0; i < len(input); i++ {
+		var currentCharacter string = string(input[i])
+		if currentCharacter != preCharacter && countShots != 0 && countRevenges != 0 {
+			if countShots > countRevenges {
+				break
+			}
+
+			countShots = 0
+			countRevenges = 0
+		}
+
+		if currentCharacter == shot {
+			countShots++
+		} else if currentCharacter == revenge {
+			countRevenges++
+		}
+		preCharacter = string(input[i])
+	}
+	if countShots > countRevenges {
+		output = badBoy
+	}
 }
